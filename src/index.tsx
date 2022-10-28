227246e7fae8943e262b79758bfdc4992ae36c5e
qirtudgus <qirtudgus@naver.com>
2022-10-28 23:33:07 +0900

    브라우저라우터 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from './layout/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <GlobalStyles />
+    <App />
+  </BrowserRouter>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
