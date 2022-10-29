266b99d11dd73ee4537a63ab75b0d880ab92a459
qirtudgus <qirtudgus@naver.com>
2022-10-29 23:41:19 +0900

    Resetcss, Routes 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './page/Home';
+import Layout from './layout/Layout';
 import GlobalStyles from './layout/GlobalStyles';
-import customAxios from './util/customAxios';
-function App() {
-  const [test, setTest] = useState('');
 
+function App() {
   return (
     <>
       <GlobalStyles />
-      <button
-        onClick={() => {
-          customAxios('post', '/test', { test: '프론트에서 보내는 테스트값' }).then((res) => {
-            console.log(res.data);
-            setTest(res.data[0].name);
-          });
-        }}
-      >
-        zzz
-      </button>
-      <p>{test}</p>
-      <p>자동배포가 되었느냐</p>
-      <p>자동배포가 되었느냐2</p>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+        </Route>
+      </Routes>
     </>
   );
 }
