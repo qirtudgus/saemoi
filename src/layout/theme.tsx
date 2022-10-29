5ba86ecd6f00c57b7ebb11545bfd87c9cfaf5292
qirtudgus <qirtudgus@naver.com>
2022-10-30 07:54:20 +0900

    테마 생성

---
+++ src/layout/theme.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+//참고사이트
+// https://wonit.tistory.com/366
+
+// 반응형 디자인을 위한 픽셀 컨버팅 함수
+const pixelToRem = (size: number) => `${size / 16}rem`;
+
+// font size를 객체로 반환해주자.
+const fontSizes = {
+  title: pixelToRem(60),
+  subtitle: pixelToRem(30),
+  paragraph: pixelToRem(18),
+};
+
+// 자주 사용하는 색을 객체로 만들자.
+const colors = {
+  main: '#3e55ff',
+  black: '#000000',
+  grey: '#999999',
+  green: '#3cb46e',
+  blue: '#000080',
+};
+
+// 자주 사용하는 스타일 속성을 theme으로 만들어보자.
+const common = {
+  flexCenter: `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  flexCenterColumn: `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+};
+
+// theme 객체에 감싸서 반환한다.
+const theme = {
+  fontSizes,
+  colors,
+  common,
+};
+
+export default theme;
