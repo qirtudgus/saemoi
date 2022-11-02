55ccdf39ecc7d9977611933ea317d20d696e4c7f
qirtudgus <qirtudgus@naver.com>
2022-11-03 00:11:29 +0900

    이벤트카드 생성 이벤트를 보여주는 제일작은 단위의 카드 컴포넌트

---
+++ src/components/EventCard.tsx
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+const EventCardWrap = styled.div`
+  position: relative;
+  width: 300px;
+  height: 210px;
+  border-radius: 20px;
+  background: green;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  overflow: hidden;
+  margin-right: 30px;
+  &:last-child {
+    margin-right: 0px;
+  }
+`;
+
+const EventCardImgWrap = styled.div`
+  width: 100%;
+  height: 130px;
+  background: #b4b4b4;
+`;
+
+const EventCardTextWrap = styled.div`
+  width: 95%;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const EventCardTitle = styled.h1`
+  position: relative;
+  font-weight: bold;
+  font-size: 1.2em;
+  padding: 5px 0;
+`;
+const EventCardDate = styled.h1`
+  position: relative;
+`;
+const EventCardDday = styled.p`
+  position: relative;
+  padding: 5px 0;
+`;
+
+const EventCard = () => {
+  return (
+    <>
+      <EventCardWrap>
+        <EventCardImgWrap></EventCardImgWrap>
+        <EventCardTextWrap>
+          <EventCardTitle>이번달 생일인 올리브라면?🎂</EventCardTitle>
+          <EventCardDate>2022.12.12 - 2022.01.01</EventCardDate>
+          <EventCardDday>{1}일 남음</EventCardDday>
+        </EventCardTextWrap>
+      </EventCardWrap>
+    </>
+  );
+};
+
+export default EventCard;
