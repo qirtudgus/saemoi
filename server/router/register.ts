7c799c133ee5713adf65d78698e0d19dc02db66a
qirtudgus <qirtudgus@naver.com>
2022-11-06 13:44:11 +0900

    회원가입 라우터 생성

--- server/router/register.ts
+++ server/router/register.ts
@@ -3,6 +3,9 @@ import express, { Request, Response, NextFunction } from 'express';
 export const registerRouter = express.Router();
 
 registerRouter.post('/join', (req, res) => {
-  console.log(req.body);
+  const { id, password, confirmPassword, nickname } = req.body;
+
+  console.log(id, password, confirmPassword, nickname);
+
   res.status(200).json({ a: 'haha' });
 });
