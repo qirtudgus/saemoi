3e1fa16a3a63204a51af30decb739365dc2193ad
qirtudgus <qirtudgus@naver.com>
2022-11-12 21:27:58 +0900

    자동로그인 시 유저정보를 불러오는 함수 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,37 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import axios from 'axios';
+import { Cookies } from 'react-cookie';
+import { UserServiceAutoLogin } from './store/userSlice';
+
+const cookies = new Cookies();
+
+//로그인 유지를 위한 함수, 토큰이 유효할 시 정보를 불러오는 디스패치, 토큰이 없을 시 return
+function loadUser() {
+  console.log(cookies.get('AT'));
+  let AT = cookies.get('AT');
+  store.dispatch(UserServiceAutoLogin.getUserAutoLogin(AT));
+  // try {
+  //   let user: string | null = localStorage.getItem('token');
+  //   if (!user) return;
+  //   axios.defaults.headers.common['Authorization'] = user;
+  //   store.dispatch();
+  // } catch (e) {
+  //   console.log(e);
+  // }
+}
+loadUser();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
