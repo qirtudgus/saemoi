fe4cd4866d3b8a65d305bf9600f8b6ad4f231fb7
qirtudgus <qirtudgus@naver.com>
2022-11-12 21:31:10 +0900

    액세스 또는 리프레쉬토큰을 생성해주는 함수

---
+++ src/util/createAccessToken.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+const SECRET_TOKEN: string = process.env.SECRET_TOKEN!;
+type TokenType = string | unknown;
+
+export const createAccessToken = (id: string | any): TokenType => {
+  try {
+    console.log(`${id}님의 토큰 생성`);
+    return jwt.sign({ id }, SECRET_TOKEN, { expiresIn: '5s' });
+  } catch (err) {
+    return err;
+  }
+};
+
+export const createRefreshToken = (id: string | any): TokenType => {
+  try {
+    console.log(`${id}님의 토큰 생성`);
+    return jwt.sign({}, SECRET_TOKEN, { expiresIn: '10s' });
+  } catch (err) {
+    return err;
+  }
+};
+
+export default createAccessToken;
