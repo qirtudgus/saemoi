6d49081e55f2a75d1ed9fa45ac94462e9aaffe5e
qirtudgus <qirtudgus@naver.com>
2022-11-12 21:33:22 +0900

    토큰검증 함수, 만료 시 null 리턴

---
+++ src/util/verifyToken.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+const SECRET_TOKEN: string = process.env.SECRET_TOKEN!;
+type TokenType = string | unknown;
+
+//유효할 시 토큰을 분해한값을, 만료 시 null 리턴
+const verifyToken = (token: string): TokenType => {
+  try {
+    return jwt.verify(token, SECRET_TOKEN);
+  } catch (err) {
+    return null;
+  }
+};
+
+export default verifyToken;
