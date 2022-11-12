a6a97da573c3a513c79def1739cc0b55e0f2fd50
qirtudgus <qirtudgus@naver.com>
2022-11-13 06:59:52 +0900

    axios 커스텀훅 생성

---
+++ src/util/useAxios.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const DOMAIN_ENV = process.env.REACT_APP_SERVER_API;
+axios.defaults.withCredentials = true;
+
+const useAxios = (initMethod: string, initUrl: string) => {
+  const navigate = useNavigate();
+  const [method, setMethod] = useState(initMethod);
+  const [url, setUrl] = useState(initUrl);
+
+  const [data, setData] = useState<any | null>();
+  const fetch = async (): Promise<any> => {
+    await axios({
+      method,
+      url: DOMAIN_ENV + '/api' + url,
+      data,
+    })
+      .then((res) => {
+        console.log('커스텀훅으로 받아온 데이터');
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+        console.log(err.response.data);
+        console.log(err.response.status);
+        alert('토큰이 만료되었습니다. 로그인 후 이용해주세요!');
+        navigate('/');
+      });
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return [data];
+};
+export default useAxios;
