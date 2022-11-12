5dfe2002f1ca1b6b710baddc8a0cdd6fe04ba13a
qirtudgus <qirtudgus@naver.com>
2022-11-12 21:31:31 +0900

    에러핸들링 추가 401 응답받을 시 토큰이 만료된것으로 간주 로그아웃 시킨다. 쿠키와 DB쿠키는 서버에서 삭제해줌.

--- src/util/customAxios.ts
+++ src/util/customAxios.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const DOMAIN_ENV = process.env.REACT_APP_SERVER_API;
-
+axios.defaults.withCredentials = true;
 export const customAxios = async (method: string, url: string, data?: any): Promise<any> => {
   return await axios({
     method,
     url: DOMAIN_ENV + '/api' + url,
     data,
+  }).catch((err) => {
+    console.log(err.response);
+    console.log(err.response.data);
+    console.log(err.response.status);
+    alert('토큰이 ㅏㅁㄴ료로딩');
+    window.location.replace('/login');
   });
 };
 export default customAxios;
