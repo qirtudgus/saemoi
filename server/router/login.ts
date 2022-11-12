651da402dd5404ff6c6d39ee9ffe7897eeae0d9a
qirtudgus <qirtudgus@naver.com>
2022-11-12 21:27:44 +0900

    로그인과, 자동로그인 api 추가

--- server/router/login.ts
+++ server/router/login.ts
@@ -1,10 +1,90 @@
 import express, { Request, Response, NextFunction } from 'express';
-import createHashPassword from '../../src/util/createHashPassword.js';
+import checkHashPassword from '../../src/util/checkHashPassword.js';
+import { createAccessToken, createRefreshToken } from '../../src/util/createAccessToken.js';
+import verifyToken from '../../src/util/verifyToken.js';
 import { db } from '../saemoi_db.js';
 export const loginRouter = express.Router();
 
-loginRouter.post('/', (req, res) => {
-  console.log(req.body.id);
-  console.log(req.body.pw);
-  res.send('haha');
+const findUserQuery = 'SELECT * FROM users WHERE id = ?';
+const insertTokenQuery = 'UPDATE users SET refreshToken = ? WHERE id = ?';
+
+loginRouter.post('/', async (req, res, next) => {
+  const { id, pw } = req.body;
+  db.query(findUserQuery, [id], (err, result) => {
+    if (result[0] === undefined) {
+      console.log('없는 아이디야');
+      res.status(200).json({ error: '없는 아이디야.' });
+    } else {
+      console.log('있는 아이디네 비밀번호 체크하자');
+      let hashResult = checkHashPassword(pw, result[0].password, result[0].salt);
+      console.log(`비밀번호 확인 결과 ${hashResult}`);
+      if (hashResult === false) {
+        console.log('비밀번호가 틀렸어.');
+        res.status(200).json({ error: '틀린 비밀번호야.' });
+      } else {
+        let nickname = result[0].nickname;
+
+        console.log('로그인 체크 성공!');
+        const AT = createAccessToken(req.body.id) as string;
+        const RT = createRefreshToken(req.body.id);
+        req.decoded = verifyToken(AT);
+        console.log(req.decoded);
+        //쿠키설정
+        res.cookie('id', id);
+        res.cookie('AT', AT);
+        res.cookie('RT', RT);
+        res.cookie('nickname', nickname);
+        console.log(req.cookies);
+        console.log(req.cookies.AT);
+        console.log(req.cookies.RT);
+        console.log(req.cookies.nickname);
+
+        db.query(insertTokenQuery, [RT, id]);
+        res.status(200).json({ id, AT, RT, isLogin: true, nickname });
+        // next();
+      }
+    }
+  });
+});
+
+loginRouter.post('/autologin', async (req, res, next) => {
+  const { AT } = req.body;
+  console.log('자동로그인이 받아온 AT');
+  console.log(AT);
+  console.log('자동로그인이 받아온 쿠키의 토큰');
+  console.log(req.cookies.AT);
+  console.log(req.cookies.id);
+
+  let ATresult = verifyToken(req.cookies.AT);
+  let RTresult = verifyToken(req.cookies.RT);
+  console.log('자동 로그인 토큰 만료 유무');
+  console.log(ATresult);
+  if (ATresult === null) {
+    if (RTresult === null) {
+      res.clearCookie('id');
+      res.clearCookie('AT');
+      res.clearCookie('RT');
+      res.clearCookie('nickname');
+      res.status(401).json({ id: '', AT: '', RT: '', isLogin: false, nickname: '' });
+    } else {
+      const newAT = createAccessToken(req.cookies.id) as string;
+      res.cookie('AT', newAT);
+      req.cookies.AT = newAT;
+      res.status(200).json({
+        id: req.cookies.id,
+        AT: req.cookies.AT,
+        RT: req.cookies.RT,
+        isLogin: true,
+        nickname: req.cookies.nickname,
+      });
+    }
+  } else {
+    res.status(200).json({
+      id: req.cookies.id,
+      AT: req.cookies.AT,
+      RT: req.cookies.RT,
+      isLogin: true,
+      nickname: req.cookies.nickname,
+    });
+  }
 });
