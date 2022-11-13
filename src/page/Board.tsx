b8453bade5b661f72db3f7f1ccbd1e45076fdf05
qirtudgus <qirtudgus@naver.com>
2022-11-14 08:50:47 +0900

    게시판 컴포넌트 추가 게시물을 클릭과 동시에 게시물데이터 받아오는 디스패치를 실행해준다.

--- src/page/Board.tsx
+++ src/page/Board.tsx
@@ -1,7 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { BoardViewService } from '../store/boardSlice';
+import { useAppDispatch } from '../store/store';
+import customAxios from '../util/customAxios';
+
 const Board = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [list, setList] = useState([
+    {
+      index: '',
+      title: '',
+    },
+  ]);
+
+  useEffect(() => {
+    customAxios('get', '/board', {}).then((res) => {
+      console.log(res.data);
+      setList(res.data);
+    });
+  }, []);
+
   return (
     <>
       <h1>게시판</h1>
+      <button
+        onClick={() => {
+          navigate('/write');
+        }}
+      >
+        작성하기
+      </button>
+      {list.map((i) => {
+        return (
+          <React.Fragment key={i.index}>
+            <li
+              onClick={() => {
+                //여기서 디스패치해서 제목과 콘텐츠를 가져와야할듯?
+                dispatch(BoardViewService.getBoard({ number: i.index }));
+                navigate(`/view/${i.index}`);
+              }}
+            >
+              <h1>{i.index}</h1>
+              <h1>{i.title}</h1>
+            </li>
+          </React.Fragment>
+        );
+      })}
     </>
   );
 };
