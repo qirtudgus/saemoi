0d4efaf147f66a64b2652898b72aed2356b118aa
qirtudgus <qirtudgus@naver.com>
2022-11-14 08:48:51 +0900

    게시판라우터 추가

---
+++ server/router/board.ts
@@ -0,0 +1,38 @@
+import express, { Request, Response, NextFunction } from 'express';
+import { db } from '../saemoi_db.js';
+export const boardRouter = express.Router();
+
+boardRouter.get('/', (req, res) => {
+  let board = 'SELECT (`index`), title FROM board';
+  db.query(board, [], (err, rows) => {
+    console.log(err);
+    console.log(rows);
+    res.send(rows);
+  });
+});
+
+boardRouter.post('/', (req, res) => {
+  let { title, content } = req.body;
+  let insertQuery = 'INSERT INTO board (title,content) VALUES (?,?)';
+  db.query(insertQuery, [title, content], (err, rows) => {
+    console.log('게시물 등록 완료');
+    console.log(err);
+    console.log(rows);
+    res.status(200).json('등록완료');
+  });
+});
+
+boardRouter.get('/view', (req, res) => {
+  let board = 'SELECT * FROM board WHERE (`index` = ?)';
+  let boardNumber = req.query.number;
+  interface BoardInterface {
+    title: string;
+    content: string;
+  }
+  db.query(board, [boardNumber], (err, rows) => {
+    console.log(err);
+    console.log(rows[0]);
+    let contentResult: BoardInterface = rows[0];
+    res.send(contentResult);
+  });
+});
