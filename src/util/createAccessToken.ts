d25d6750561d5dc05640f1fb217ce00384c5f73d
qirtudgus <qirtudgus@naver.com>
2022-11-14 08:52:37 +0900

    토큰유효시간 변경

--- src/util/createAccessToken.ts
+++ src/util/createAccessToken.ts
@@ -5,7 +5,7 @@ type TokenType = string | unknown;
 export const createAccessToken = (id: string | any): TokenType => {
   try {
     console.log(`${id}님의 토큰 생성`);
-    return jwt.sign({ id }, SECRET_TOKEN, { expiresIn: '5s' });
+    return jwt.sign({ id }, SECRET_TOKEN, { expiresIn: '10h' });
   } catch (err) {
     return err;
   }
@@ -14,7 +14,7 @@ export const createAccessToken = (id: string | any): TokenType => {
 export const createRefreshToken = (id: string | any): TokenType => {
   try {
     console.log(`${id}님의 토큰 생성`);
-    return jwt.sign({}, SECRET_TOKEN, { expiresIn: '10s' });
+    return jwt.sign({}, SECRET_TOKEN, { expiresIn: '360h' });
   } catch (err) {
     return err;
   }
