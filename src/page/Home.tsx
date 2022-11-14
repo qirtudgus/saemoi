41cfd8a2a6a93004815b95387f47072ec7bd1a79
qirtudgus <qirtudgus@naver.com>
2022-11-14 21:59:06 +0900

    스타일드컴포넌트 as 테스트

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,12 +1,17 @@
 import { useAppSelector } from '../store/store';
 import customAxios from '../util/customAxios';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 3rem;
+`;
 
 const Home = () => {
   const nickname = useAppSelector((state) => state.user.nickname);
 
   return (
     <div>
-      <h1>당신의 닉네임 {nickname}</h1>
+      <Title as={'h3'}>당신의 닉네임 {nickname}</Title>
       <button
         onClick={() => {
           customAxios('post', '/test', { test: '프론트에서 보내는 테스트값' }).then((res) => {
