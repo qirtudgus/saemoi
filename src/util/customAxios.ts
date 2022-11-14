c643893d102f13c0df6c0a320e8ceb03ce2fa7d7
qirtudgus <qirtudgus@naver.com>
2022-11-14 22:04:51 +0900

    메소드타입을 따로 선언하여 지정

--- src/util/customAxios.ts
+++ src/util/customAxios.ts
@@ -1,16 +1,15 @@
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+
+type AxiosMethod = 'post' | 'get' | 'put' | 'delete';
 
 const DOMAIN_ENV = process.env.REACT_APP_SERVER_API;
 axios.defaults.withCredentials = true;
-export const customAxios = async (method: string, url: string, data?: any): Promise<any> => {
+export const customAxios = async (method: AxiosMethod, url: string, data?: any): Promise<any> => {
   return await axios({
     method,
     url: DOMAIN_ENV + '/api' + url,
     data,
   }).catch((err) => {
-    console.log(err.response);
-    console.log(err.response.data);
     console.log(err.response.status);
     if (err.response.status === 401) {
       alert('토큰이 만료되었습니다. 로그인 후 이용해주세요!');
