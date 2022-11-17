d9bfa90e3945df08af92fd69c88020077a6bb699
qirtudgus <qirtudgus@naver.com>
2022-11-17 13:19:05 +0900

    댓글 기능버튼 클릭 시 토글, 외부영역 클릭 시 제거

--- src/page/View.tsx
+++ src/page/View.tsx
@@ -11,6 +11,7 @@ import { elapsedTime, returnTodayString } from '../util/returnTodayString';
 import TitleText from '../components/TitleText';
 import { BasicButton, SolidButton } from '../components/BtnGroup';
 import theme from '../layout/theme';
+import more_horiz from '../img/more_horiz.svg';
 
 const ViewWrap = styled.div`
   height: auto;
@@ -37,7 +38,7 @@ const UserInfoWrap = styled.div`
 
   & .editDate {
     margin-top: 5px;
-    color: #aaa;
+    color: #888;
     font-size: 0.9em;
   }
 `;
@@ -48,18 +49,54 @@ const ContentUserInfo = styled.div`
 
 const CommentWrap = styled.div`
   position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: 1px solid#dadde6;
+  border-radius: 5px;
+  height: 220px;
+  padding: 10px 0;
+  margin-top: 30px;
 `;
 
 const CommentArea = styled.textarea`
   position: relative;
   padding: 10px;
-  border: 1px solid #ddd;
+  height: 150px;
+  border: 1px solid #dadde6;
   border-radius: 2px;
+  width: 98.5%;
   &:hover {
     border: 1px solid #000;
   }
   &:focus {
     outline: none;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    width: 97.5%;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 93.5%;
+  }
+`;
+
+const SolidButtonWrap = styled.div`
+  width: 98.5%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+  & span {
+    color: #888;
+    word-break: keep-all;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
+    width: 97.5%;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 93.5%;
   }
 `;
 
@@ -88,14 +125,75 @@ const CommentList = styled.div`
 `;
 
 const MainComment = styled.div`
-  padding: 15px 0;
+  padding: 15px;
   border-bottom: 1px solid#dadde6;
 `;
+
 const CommentUserInfo = styled.div`
   display: flex;
+  justify-content: space-between;
+  height: 20px;
 `;
+
+const NicknameAndTime = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
 const CommentContent = styled.div`
   padding: 5px 0 10px 0;
+`;
+
+const MoreButtonGroup = styled.div`
+  width: auto;
+  cursor: pointer;
+
+  z-index: 2;
+  width: 20px;
+  height: 20px;
+  display: flex;
+  position: relative;
+  align-items: center;
+  & img {
+    position: relative;
+    z-index: 1;
+    width: 20px;
+    pointer-events: none;
+  }
+  & img:hover {
+    opacity: 0.8;
+  }
+  & ul {
+    background-color: #fff;
+
+    z-index: 10;
+    width: 100px;
+    height: 70px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    position: absolute;
+    top: 5px;
+    right: 22px;
+
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    display: none;
+  }
+  &.active ul {
+    display: flex;
+  }
+  & li {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  & li:hover {
+    font-weight: bold;
+    background-color: #eee;
+  }
 `;
 
 const View = () => {
@@ -103,6 +201,7 @@ const View = () => {
   const [undifinedContent, setUndifinedContent] = useState(false);
   const [clickCommentIndex, setClickCommentIndex] = useState<number>(-1);
   const [writeCommentAfterRendering, setWriteCommentAfterRendering] = useState(false);
+
   let { number } = useParams();
   const id = useAppSelector((state) => state.user.id);
   const nickname = useAppSelector((state) => state.user.nickname);
@@ -180,6 +279,22 @@ const View = () => {
       }
     });
   }, [number, writeCommentAfterRendering]);
+
+  useEffect(() => {
+    let deleteModal = (e: any) => {
+      console.log(e.target.className);
+      if (e.target.className.includes('ulList active')) {
+        console.log('액티브가 있네요');
+      } else {
+        let a = document.querySelectorAll('.ulList');
+        a.forEach((i) => {
+          i.classList.remove('active');
+        });
+      }
+    };
+    document.addEventListener('click', deleteModal);
+    return () => document.removeEventListener('click', deleteModal);
+  });
 
   /*
   이미 삭제된 게시물에 대한 예외처리 꼭 해주자!!!!!!!!!!!!!
@@ -262,13 +377,102 @@ const View = () => {
                         {i.depth === '0' ? (
                           <MainComment>
                             <CommentUserInfo>
-                              <span>{i.nickname}</span>
-                              <VerticalLine />
-                              <span>{elapsedTime(i.date)}</span>
+                              <NicknameAndTime>
+                                <span>{i.nickname}</span>
+                                <VerticalLine />
+                                <span>{elapsedTime(i.date)}</span>
+                              </NicknameAndTime>
+                              <MoreButtonGroup
+                                className='ulList'
+                                onClick={(e) => {
+                                  if (e.currentTarget.className.includes('ulList active')) {
+                                    e.currentTarget.classList.remove('active');
+                                  } else {
+                                    document.querySelectorAll('.ulList').forEach((i) => {
+                                      i.classList.remove('active');
+                                    });
+                                    e.currentTarget.classList.toggle('active');
+                                  }
+                                }}
+                              >
+                                <img
+                                  src={more_horiz}
+                                  alt='더보기'
+                                />
+                                <ul>
+                                  <li
+                                    onClick={() => {
+                                      alert('수정하시겠습니가?');
+                                    }}
+                                  >
+                                    수정하기
+                                  </li>
+                                  <li
+                                    onClick={() => {
+                                      console.log(`${i.index}번 댓글 삭제 시도`);
+                                      if (window.confirm('해당 댓글을 삭제하시겠습니까?')) {
+                                        customAxios(
+                                          'put',
+                                          `/comment?commentNumber=${i.index}&&boardNumber=${number}`,
+                                          {},
+                                        ).then((res) => {
+                                          if (res.status === 200) {
+                                            setWriteCommentAfterRendering((prev) => !prev);
+                                            document.querySelectorAll('.ulList').forEach((i) => {
+                                              i.classList.remove('active');
+                                            });
+                                          }
+                                        });
+                                      }
+                                    }}
+                                  >
+                                    삭제하기
+                                  </li>
+                                </ul>
+                              </MoreButtonGroup>
                             </CommentUserInfo>
                             <CommentContent>
                               <span>{i.content}</span>
                             </CommentContent>
+                            {i.depth === '0' && (
+                              <span
+                                onClick={() => {
+                                  if (clickCommentIndex === index) {
+                                    setClickCommentIndex(-1);
+                                  } else {
+                                    setClickCommentIndex(index);
+                                  }
+
+                                  console.log(clickCommentIndex);
+                                }}
+                              >
+                                답글
+                              </span>
+                            )}
+
+                            {/* 사용자가 같으며, 댓글이 삭제되지않은 상태일때만 삭제버튼 렌더링 */}
+                            {i.id === id && i.isDeleted === 'false' && (
+                              <>
+                                <button
+                                  onClick={() => {
+                                    console.log(`${i.index}번 댓글 삭제 시도`);
+                                    if (window.confirm('해당 댓글을 삭제하시겠습니까?')) {
+                                      customAxios(
+                                        'put',
+                                        `/comment?commentNumber=${i.index}&&boardNumber=${number}`,
+                                        {},
+                                      ).then((res) => {
+                                        if (res.status === 200) {
+                                          setWriteCommentAfterRendering((prev) => !prev);
+                                        }
+                                      });
+                                    }
+                                  }}
+                                >
+                                  삭제
+                                </button>
+                              </>
+                            )}
                           </MainComment>
                         ) : (
                           <>
@@ -277,15 +481,9 @@ const View = () => {
                             ) : (
                               <>
                                 <TitleText text='대댓글'></TitleText>
-                                <p>
-                                  <span>{i.nickname}</span>
-                                </p>
-                                <p>
-                                  <span>{i.content}</span>
-                                </p>
-                                <p>
-                                  <span>{i.date}</span>
-                                </p>
+                                <p>{i.nickname}</p>
+                                <p>{i.content}</p>
+                                <p>{i.date}</p>
                               </>
                             )}
                           </>
@@ -331,39 +529,6 @@ const View = () => {
                             </button>
                           </>
                         ) : null}
-                        {i.depth === '0' && (
-                          <button
-                            onClick={() => {
-                              setClickCommentIndex(index);
-                              console.log(clickCommentIndex);
-                            }}
-                          >
-                            대댓글
-                          </button>
-                        )}
-                        {/* 사용자가 같으며, 댓글이 삭제되지않은 상태일때만 삭제버튼 렌더링 */}
-                        {i.id === id && i.isDeleted === 'false' && (
-                          <>
-                            <button
-                              onClick={() => {
-                                console.log(`${i.index}번 댓글 삭제 시도`);
-                                if (window.confirm('해당 댓글을 삭제하시겠습니까?')) {
-                                  customAxios(
-                                    'put',
-                                    `/comment?commentNumber=${i.index}&&boardNumber=${number}`,
-                                    {},
-                                  ).then((res) => {
-                                    if (res.status === 200) {
-                                      setWriteCommentAfterRendering((prev) => !prev);
-                                    }
-                                  });
-                                }
-                              }}
-                            >
-                              삭제
-                            </button>
-                          </>
-                        )}
                       </React.Fragment>
                     );
                   })}
@@ -377,29 +542,31 @@ const View = () => {
                 cols={40}
                 rows={5}
               ></CommentArea>
+              <SolidButtonWrap>
+                <span>건강한 댓글을 달면 모두가 건강해져요!</span>
+                <SolidButton
+                  text='댓글 등록'
+                  OnClick={() => {
+                    console.log(commentRef.current?.value);
+                    //<br>태그로 치환하지않고 그냥 db에 넣었다빼는것이 비용이 덜 들겠다.
+                    //치환하고나면 다시 여기와서 \n으로 치환해줘야하는데, 댓글이 많을수록 비용도 곱해진다.
+                    // let content = commentRef.current?.value.replaceAll('\n', '<br>');
+                    let content2 = commentRef.current?.value;
+                    let date = returnTodayString();
+                    customAxios('post', '/comment/write', { board_index: number, content2, date, id, nickname }).then(
+                      (res) => {
+                        if (res.status === 200) {
+                          alert('댓글이 등록되었습니다!');
+                          setWriteCommentAfterRendering((prev) => !prev);
+                          commentRef.current!.value = '';
+                          console.log(res.data);
+                        }
+                      },
+                    );
+                  }}
+                ></SolidButton>
+              </SolidButtonWrap>
             </CommentWrap>
-
-            <SolidButton
-              text='댓글 등록'
-              OnClick={() => {
-                console.log(commentRef.current?.value);
-                //<br>태그로 치환하지않고 그냥 db에 넣었다빼는것이 비용이 덜 들겠다.
-                //치환하고나면 다시 여기와서 \n으로 치환해줘야하는데, 댓글이 많을수록 비용도 곱해진다.
-                // let content = commentRef.current?.value.replaceAll('\n', '<br>');
-                let content2 = commentRef.current?.value;
-                let date = returnTodayString();
-                customAxios('post', '/comment/write', { board_index: number, content2, date, id, nickname }).then(
-                  (res) => {
-                    if (res.status === 200) {
-                      alert('댓글이 등록되었습니다!');
-                      setWriteCommentAfterRendering((prev) => !prev);
-                      commentRef.current!.value = '';
-                      console.log(res.data);
-                    }
-                  },
-                );
-              }}
-            ></SolidButton>
           </>
         )}
       </ViewWrap>
