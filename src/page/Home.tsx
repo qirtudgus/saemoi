f7a4a48a99af69688909ef99d714c27b51d5db4b
qirtudgus <qirtudgus@naver.com>
2022-12-03 17:09:55 +0900

    현재 접속자를 스토어에서 가져와서 출력

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,4 @@
-import { useAppSelector } from '../store/store';
+import { useAppSelector, useAppDispatch } from '../store/store';
 import styled from 'styled-components';
 
 const Title = styled.h1`
@@ -7,9 +7,14 @@ const Title = styled.h1`
 
 const Home = () => {
   const nickname = useAppSelector((state) => state.user.nickname);
+  const list = useAppSelector((state) => state.userList);
 
   return (
     <>
+      <p>현재 접속자 : {list.length}</p>
+      {list.map((i: any) => {
+        return <li key={i.id}>{i.id}</li>;
+      })}
       <div>
         <Title as={'h3'}>홈 {nickname}</Title>
       </div>
