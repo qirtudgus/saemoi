63cdd0064b1a27d140874624adafd0eebc1b3f31
qirtudgus <qirtudgus@naver.com>
2022-12-03 17:11:12 +0900

    로그인시, 자동로그인시 소켓 접속자리스트에 자신을 추가한다. 자동로그인 실패 (평상 시 접속)시 접속자 리스트만 가져온다.

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -2,6 +2,10 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import customAxios from '../util/customAxios';
 import { Cookies } from 'react-cookie';
+import { io } from 'socket.io-client';
+const port = process.env.REACT_APP_IO_SERVER_API as string;
+const socket = io(port);
+
 const cookies = new Cookies();
 //비동기함수
 export const UserService = {
@@ -69,6 +73,7 @@ const user = createSlice({
         console.log('getUser가 펜딩중');
       })
       .addCase(UserService.getUser.fulfilled, (state, actions) => {
+        //로그인 성공 시
         //actions의 인자
         // meta : 요청에 담아 보낸 데이터, 요청아이디, 요청상태
         // payload : 요청결과로 받아온 응답데이터
@@ -80,7 +85,12 @@ const user = createSlice({
         //엑시오스 헤더에 id도 담아둔다?
         console.log(actions.payload.id);
         console.log(document.cookie);
+        //로그인 시 소켓에 아이디를 담아 전송
+        socket.emit('users.count', { id: state.id }, (res: any) => {
+          console.log('로그인 에밋 후');
+        });
       })
+
       .addCase(UserService.getUser.rejected, (state) => {
         console.log('getUser가 리젝티드');
       })
@@ -88,15 +98,26 @@ const user = createSlice({
         console.log('자동로그인 시도');
       })
       .addCase(UserServiceAutoLogin.getUserAutoLogin.fulfilled, (state, actions) => {
+        //자동 로그인 성공 시
         console.log('자동로그인 완료');
         console.log(actions);
         console.log(actions.payload);
         state.isLogin = actions.payload.isLogin;
         state.id = actions.payload.id;
         state.nickname = actions.payload.nickname;
+        //로그인 시 소켓에 아이디를 담아 전송
+        socket.emit('users.count', { id: state.id }, (res: any) => {
+          console.log('로그인 에밋 후');
+          // dispatch(connectedUser([{ id, socketId: socket.id }]));
+        });
       })
       .addCase(UserServiceAutoLogin.getUserAutoLogin.rejected, (state, actions) => {
         console.log('자동로그인 실패');
+        //접속 시 소켓에 아이디를 담아 전송
+        socket.emit('users.count', { id: '첫접속' }, (res: any) => {
+          console.log('로그인 에밋 후');
+          // dispatch(connectedUser([{ id, socketId: socket.id }]));
+        });
       });
   },
 });
