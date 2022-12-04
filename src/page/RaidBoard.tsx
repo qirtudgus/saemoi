c0f6b423aec5dddac67b039bde796f2c8bc68bc1
qirtudgus <qirtudgus@naver.com>
2022-12-05 05:38:59 +0900

    pc버전의 새로고침,등록버튼 추가

--- src/page/RaidBoard.tsx
+++ src/page/RaidBoard.tsx
@@ -4,12 +4,14 @@ import customAxios from '../util/customAxios';
 import TitleText from '../components/TitleText';
 import 새로고침이미지 from '../img/refresh_white_24dp.svg';
 import 작성하기이미지 from '../img/edit_document_white_24dp.svg';
+import 등록하기 from '../img/post_add_white_24dp.svg';
 import theme from '../layout/theme';
 import { useAppSelector } from '../store/store';
 import React from 'react';
 import RaidCard from '../components/RaidCard';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
+import { SolidButton } from '../components/BtnGroup';
 
 const RefreshAni = keyframes`
   to {
@@ -21,17 +23,37 @@ const RefreshAni = keyframes`
 `;
 
 interface RefreshInterface {
-  isRefresh: boolean;
+  isRefresh?: boolean;
 }
+
+const BtnWrap = styled.div`
+  margin: 0 auto;
+  position: fixed;
+  display: none;
+  bottom: 23vh;
+  width: 95%;
+  max-width: 1280px;
+  flex-direction: column-reverse;
+  height: 115px;
+  justify-content: space-between;
+  align-items: flex-end;
+  @media ${({ theme }) => theme.device.tablet} {
+    display: flex;
+    bottom: 20px;
+  }
+  @media ${({ theme }) => theme.device.mobile} {
+    display: flex;
+  }
+`;
 
 const RefreshBtn = styled.button<RefreshInterface>`
   cursor: pointer;
-  position: fixed;
+  position: relative;
   width: 50px;
   height: 50px;
   background-color: ${({ theme }) => theme.colors.main};
-  bottom: 20px;
-  right: 20px;
+  /* bottom: 20px;
+  right: 20px; */
   border-radius: 100%;
   box-shadow: 0px 2px 4px 3px rgb(0 0 0 / 20%);
 
@@ -46,16 +68,73 @@ const RefreshBtn = styled.button<RefreshInterface>`
 
 const WriteBtn = styled.button`
   cursor: pointer;
-  position: fixed;
+  position: relative;
   width: 50px;
   height: 50px;
   background-color: ${({ theme }) => theme.colors.main};
-  bottom: 90px;
-  right: 20px;
+  /* bottom: 90px;
+  right: 20px; */
   border-radius: 100%;
   box-shadow: 0px 2px 4px 3px rgb(0 0 0 / 20%);
   & img {
     width: 70%;
+  }
+`;
+
+const PcBtnWrap = styled.div`
+  width: 100%;
+  max-width: 1280px;
+  display: flex;
+  align-items: center;
+`;
+
+const PcInnerBtnWrap = styled.div`
+  display: flex;
+`;
+const Title = styled.h1`
+  font-size: 2em;
+  padding: 15px 0;
+  display: block;
+  font-weight: bold;
+  word-break: keep-all;
+  justify-content: flex-start;
+  flex-shrink: 0;
+  @media ${({ theme }) => theme.device.tablet} {
+    font-size: 1.8em;
+  }
+
+  @media ${({ theme }) => theme.device.mobile} {
+    font-size: 1.6em;
+  }
+`;
+
+const ButtonWrapPc = styled.button<RefreshInterface>`
+  cursor: pointer;
+  width: 100px;
+  height: 35px;
+  padding: 5px;
+  border-radius: 10px;
+  background-color: ${({ theme }) => theme.colors.main};
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 15px;
+  &:hover {
+    opacity: 0.8;
+  }
+  & img {
+    max-width: 25px;
+  }
+  & img.on {
+    animation: ${RefreshAni} 0.8s ease;
+  }
+
+  @media ${({ theme }) => theme.device.tablet} {
+    display: none;
+  }
+
+  @media ${({ theme }) => theme.device.mobile} {
   }
 `;
 
@@ -66,6 +145,7 @@ const RaidBoard = () => {
   // const lists = useAppSelector((state) => state.userList);
 
   const imgRef = useRef() as RefObject<HTMLImageElement>;
+  const imgPcRef = useRef() as RefObject<HTMLImageElement>;
 
   const navigate = useNavigate();
 
@@ -84,16 +164,14 @@ const RaidBoard = () => {
     void a?.offsetWidth;
     a?.classList.add('on');
     setIsLoad((prev) => !prev);
-    // window.scrollTo({ top: 0 });
-    // setIsLoading(false);
-    // customAxios('get', '/raidboard/list', {})
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     setList(res.data);
-    //   })
-    //   .then((res) => {
-    //     setIsLoading(true);
-    //   });
+  };
+
+  const RefreshPc = () => {
+    let a = imgPcRef.current;
+    a?.classList.remove('on');
+    void a?.offsetWidth;
+    a?.classList.add('on');
+    setIsLoad((prev) => !prev);
   };
 
   return (
@@ -102,7 +180,30 @@ const RaidBoard = () => {
       {lists.map((i: any) => {
         return <li key={i.id}>{i.id}</li>;
       })} */}
-      <TitleText text='레이드 리스트'></TitleText>
+      <PcBtnWrap>
+        <Title>레이드 리스트</Title>
+        <PcInnerBtnWrap>
+          <ButtonWrapPc onClick={RefreshPc}>
+            <img
+              ref={imgPcRef}
+              src={새로고침이미지}
+              alt='새로고침'
+            />
+            새로고침
+          </ButtonWrapPc>
+          <ButtonWrapPc
+            onClick={() => {
+              console.log('test');
+            }}
+          >
+            <img
+              src={작성하기이미지}
+              alt='작성하기'
+            />
+            등록하기
+          </ButtonWrapPc>
+        </PcInnerBtnWrap>
+      </PcBtnWrap>
       {isLoading
         ? list.map((i: any, index: number) => (
             <RaidCard
@@ -117,30 +218,32 @@ const RaidBoard = () => {
             />
           ))
         : '로딩중'}
-      <RefreshBtn
-        isRefresh={isLoad}
-        onClick={Refresh}
-      >
-        <motion.img
-          ref={imgRef}
-          // animate={{ rotate: isLoad ? 360 : 0 }}
-          // transition={{ duration: 0.5 }}
-          src={새로고침이미지}
-          alt='새로고침'
-        ></motion.img>
-      </RefreshBtn>
-      <WriteBtn
-        onClick={() => {
-          navigate('/raidboard/write');
-        }}
-      >
-        <motion.img
-          // animate={{ rotate: isLoad ? 360 : 0 }}
-          // transition={{ duration: 0.5 }}
-          src={작성하기이미지}
-          alt='작성하기'
-        ></motion.img>
-      </WriteBtn>
+      <BtnWrap>
+        <RefreshBtn
+          isRefresh={isLoad}
+          onClick={Refresh}
+        >
+          <motion.img
+            ref={imgRef}
+            // animate={{ rotate: isLoad ? 360 : 0 }}
+            // transition={{ duration: 0.5 }}
+            src={새로고침이미지}
+            alt='새로고침'
+          ></motion.img>
+        </RefreshBtn>
+        <WriteBtn
+          onClick={() => {
+            navigate('/raidboard/write');
+          }}
+        >
+          <motion.img
+            // animate={{ rotate: isLoad ? 360 : 0 }}
+            // transition={{ duration: 0.5 }}
+            src={작성하기이미지}
+            alt='작성하기'
+          ></motion.img>
+        </WriteBtn>
+      </BtnWrap>
     </ThemeProvider>
   );
 };
