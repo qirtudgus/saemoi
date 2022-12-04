9ebace9f5ba6dcbcbfae3e1c995bc9a6cafec3bb
qirtudgus <qirtudgus@naver.com>
2022-12-05 02:25:03 +0900

    httponly만 적용하기로..

--- server/router/login.ts
+++ server/router/login.ts
@@ -30,10 +30,10 @@ loginRouter.post('/', async (req, res, next) => {
         req.decoded = verifyToken(AT);
         console.log(req.decoded);
         //쿠키설정
-        res.cookie('id', id, { httpOnly: true, secure: true });
-        res.cookie('AT', AT, { httpOnly: true, secure: true });
-        res.cookie('RT', RT, { httpOnly: true, secure: true });
-        res.cookie('nickname', nickname, { httpOnly: true, secure: true });
+        res.cookie('id', id, { httpOnly: true });
+        res.cookie('AT', AT, { httpOnly: true });
+        res.cookie('RT', RT, { httpOnly: true });
+        res.cookie('nickname', nickname, { httpOnly: true });
         console.log(req.cookies);
 
         db.query(insertTokenQuery, [RT, id]);
