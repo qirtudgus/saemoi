4527236bbe8ac4fda120d726027a52cc30aef130
qirtudgus <qirtudgus@naver.com>
2022-12-05 00:26:25 +0900

    새로고침 버튼 애니메이션 구현 및 작성하기 버튼 추가

--- src/page/RaidBoard.tsx
+++ src/page/RaidBoard.tsx
@@ -3,26 +3,20 @@ import { RefObject, useEffect, useRef, useState } from 'react';
 import customAxios from '../util/customAxios';
 import TitleText from '../components/TitleText';
 import 새로고침이미지 from '../img/refresh_white_24dp.svg';
+import 작성하기이미지 from '../img/edit_document_white_24dp.svg';
 import theme from '../layout/theme';
 import { useAppSelector } from '../store/store';
 import React from 'react';
 import RaidCard from '../components/RaidCard';
 import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
 
 const RefreshAni = keyframes`
   to {
     transform: rotate(0deg);
   }
   from {
-    transform: rotate(-360deg);
-  }
-`;
-const ARefreshAni222 = keyframes`
-  to {
-    transform: rotate(0deg);
-  }
-  from {
-    transform: rotate(-360deg);
+    transform: rotate(-360deg) ;
   }
 `;
 
@@ -39,27 +33,41 @@ const RefreshBtn = styled.button<RefreshInterface>`
   bottom: 20px;
   right: 20px;
   border-radius: 100%;
+  box-shadow: 0px 2px 4px 3px rgb(0 0 0 / 30%);
+
   & img {
     width: 70%;
-    /* ${(props) =>
-      props.isRefresh
-        ? css`
-            animation: ${RefreshAni} 0.8s ease;
-          `
-        : css`
-            animation: ${ARefreshAni222} 0.8s ease;
-          `} */
+  }
+
+  & img.on {
+    animation: ${RefreshAni} 0.8s ease;
+  }
+`;
+
+const WriteBtn = styled.button`
+  cursor: pointer;
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  background-color: ${({ theme }) => theme.colors.main};
+  bottom: 90px;
+  right: 20px;
+  border-radius: 100%;
+  box-shadow: 0px 2px 4px 3px rgb(0 0 0 / 30%);
+  & img {
+    width: 70%;
   }
 `;
 
 const RaidBoard = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isLoad, setIsLoad] = useState(false);
-  const [isAni, setIsAni] = useState(true);
   const [list, setList] = useState<any>(null);
   // const lists = useAppSelector((state) => state.userList);
 
   const imgRef = useRef() as RefObject<HTMLImageElement>;
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.scrollTo({ top: 0 });
@@ -71,6 +79,10 @@ const RaidBoard = () => {
   }, [isLoad]);
 
   const Refresh = () => {
+    let a = imgRef.current;
+    a?.classList.remove('on');
+    void a?.offsetWidth;
+    a?.classList.add('on');
     setIsLoad((prev) => !prev);
     // window.scrollTo({ top: 0 });
     // setIsLoading(false);
@@ -110,12 +122,25 @@ const RaidBoard = () => {
         onClick={Refresh}
       >
         <motion.img
-          animate={{ rotate: isLoad ? 360 : 0 }}
-          transition={{ duration: 0.5 }}
+          ref={imgRef}
+          // animate={{ rotate: isLoad ? 360 : 0 }}
+          // transition={{ duration: 0.5 }}
           src={새로고침이미지}
           alt='새로고침'
         ></motion.img>
       </RefreshBtn>
+      <WriteBtn
+        onClick={() => {
+          navigate('/raidboard/write');
+        }}
+      >
+        <motion.img
+          // animate={{ rotate: isLoad ? 360 : 0 }}
+          // transition={{ duration: 0.5 }}
+          src={작성하기이미지}
+          alt='작성하기'
+        ></motion.img>
+      </WriteBtn>
     </ThemeProvider>
   );
 };
