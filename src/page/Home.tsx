e7e2fef40508e8722250c0280cb5d05b1f644a82
qirtudgus <qirtudgus@naver.com>
2022-12-05 00:56:18 +0900

    홈 들어올 시 레이드리스트 출력

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,23 +1,13 @@
 import { useAppSelector, useAppDispatch } from '../store/store';
 import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 3rem;
-`;
+import RaidBoard from './RaidBoard';
 
 const Home = () => {
   const nickname = useAppSelector((state) => state.user.nickname);
-  const list = useAppSelector((state) => state.userList);
 
   return (
     <>
-      <p>현재 접속자 : {list.length}</p>
-      {list.map((i: any) => {
-        return <li key={i.id}>{i.id}</li>;
-      })}
-      <div>
-        <Title as={'h3'}>홈 {nickname}</Title>
-      </div>
+      <RaidBoard />
     </>
   );
 };
