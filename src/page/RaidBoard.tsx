d8f8b6665c936da62aec2c48c75ec7d94e68a91a
qirtudgus <qirtudgus@naver.com>
2022-12-04 13:42:01 +0900

    새로고침 로직 변경 및 레이드카드 분할

--- src/page/RaidBoard.tsx
+++ src/page/RaidBoard.tsx
@@ -1,38 +1,12 @@
 import styled, { ThemeProvider } from 'styled-components';
 import { useEffect, useState } from 'react';
 import customAxios from '../util/customAxios';
-import { elapsedTime } from '../util/returnTodayString';
 import TitleText from '../components/TitleText';
 import 새로고침이미지 from '../img/refresh_white_24dp.svg';
 import theme from '../layout/theme';
-
-const ListCard = styled.div`
-  width: 100%;
-  height: auto;
-  background-color: #575757;
-  margin-bottom: 15px;
-  border-radius: 5px;
-  box-shadow: 0px 2px 5px 0px rgb(0 0 0 / 44%);
-  padding: 10px 10px;
-  & p {
-    word-break: keep-all;
-    line-height: 1.4em;
-  }
-`;
-
-const ListTop = styled.p`
-  align-items: center;
-  display: flex;
-  margin-bottom: 5px;
-  font-size: 1.1em;
-`;
-
-const ListFooterTime = styled.span`
-  display: block;
-  width: 100%;
-  text-align: right;
-  margin-right: 0px !important;
-`;
+import { useAppSelector } from '../store/store';
+import React from 'react';
+import RaidCard from '../components/RaidCard';
 
 const RefreshBtn = styled.button`
   cursor: pointer;
@@ -48,72 +22,64 @@ const RefreshBtn = styled.button`
   }
 `;
 
-const VerticalLine = styled.span`
-  display: inline-block;
-  border-left: 1px solid #dadde6;
-  margin: 0 10px;
-  height: 10px;
-`;
-
 const RaidBoard = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoad, setIsLoad] = useState(false);
   const [list, setList] = useState<any>(null);
+  const lists = useAppSelector((state) => state.userList);
 
   useEffect(() => {
+    window.scrollTo({ top: 0 });
     customAxios('get', '/raidboard/list', {}).then((res) => {
       console.log(res.data);
       setList(res.data);
       setIsLoading(true);
     });
-  }, []);
+  }, [isLoad]);
 
   const Refresh = () => {
-    window.scrollTo({ top: 0 });
-    setIsLoading(false);
-    customAxios('get', '/raidboard/list', {})
-      .then((res) => {
-        console.log(res.data);
-        setList(res.data);
-
-        // setTimeout(() => setIsLoading(true), 330);
-      })
-      .then((res) => {
-        setIsLoading(true);
-      });
+    setIsLoad((prev) => !prev);
+    // window.scrollTo({ top: 0 });
+    // setIsLoading(false);
+    // customAxios('get', '/raidboard/list', {})
+    //   .then((res) => {
+    //     console.log(res.data);
+    //     setList(res.data);
+    //   })
+    //   .then((res) => {
+    //     setIsLoading(true);
+    //   });
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <>
-        <TitleText text='레이드 리스트'></TitleText>
-        {isLoading
-          ? list.map((i: any, index: number) => (
-              <ListCard key={index}>
-                <ListTop>
-                  <span>{i.raidDifficulty}</span>
-                  <VerticalLine />
-                  <span>{i.monsterName}</span>
-                  <VerticalLine />
-                  <span>{i.type}</span>
-                  <VerticalLine />
-                  <span>{i.raidCode}</span>
-
-                  <ListFooterTime> {elapsedTime(i.date)}</ListFooterTime>
-                </ListTop>
-                <p>{i.raidText}</p>
-                <p>{i.raidOption}</p>
-              </ListCard>
-            ))
-          : '로딩중'}
-        <RefreshBtn onClick={Refresh}>
-          <img
-            src={새로고침이미지}
-            alt='새로고침'
-          />
-        </RefreshBtn>
-      </>
+      <p>현재 접속자 : {lists.length}</p>
+      {lists.map((i: any) => {
+        return <li key={i.id}>{i.id}</li>;
+      })}
+      <TitleText text='레이드 리스트'></TitleText>
+      {isLoading
+        ? list.map((i: any, index: number) => (
+            <RaidCard
+              key={i.idx}
+              monsterName={i.monsterName}
+              raidDifficulty={i.raidDifficulty}
+              raidCode={i.raidCode}
+              type={i.type}
+              date={i.date}
+              raidText={i.raidText}
+              raidOption={i.raidOption}
+            />
+          ))
+        : '로딩중'}
+      <RefreshBtn onClick={Refresh}>
+        <img
+          src={새로고침이미지}
+          alt='새로고침'
+        />
+      </RefreshBtn>
     </ThemeProvider>
   );
 };
 
-export default RaidBoard;
+export default React.memo(RaidBoard);
