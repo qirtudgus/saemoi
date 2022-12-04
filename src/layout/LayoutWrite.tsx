9643db4744d090dbc726a75916a5e8dde7b09658
qirtudgus <qirtudgus@naver.com>
2022-12-05 08:08:43 +0900

    헤더가 따로 필요없거나, 그에 맞게 추가해야하는 레이아웃 컴포넌트 생성 레이드 작성 시 그에 맞는 ui를 적용하기위해 사용했다.

---
+++ src/layout/LayoutWrite.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme';
+
+import React, { useRef } from 'react';
+
+const MainWrap = styled.main`
+  min-height: calc(100vh - 310px);
+  width: 95%;
+  max-width: 1280px;
+  margin: 0 auto;
+`;
+
+const LayoutWrite = () => {
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <MainWrap>
+          <Outlet />
+        </MainWrap>
+      </ThemeProvider>
+    </div>
+  );
+};
+
+export default React.memo(LayoutWrite);
