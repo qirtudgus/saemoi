51ec52a7e58356068773599375cdb507e68e0634
qirtudgus <qirtudgus@naver.com>
2022-12-05 13:38:00 +0900

    게시글 수정 컴포넌트 디자인 추가

--- src/page/PostEdit.tsx
+++ src/page/PostEdit.tsx
@@ -6,6 +6,29 @@ import customAxios from '../util/customAxios';
 import { useAppSelector } from '../store/store';
 import { Editor } from '@toast-ui/react-editor';
 import { returnTodayString } from '../util/returnTodayString';
+import styled from 'styled-components';
+import { BasicButton, SolidButton } from '../components/BtnGroup';
+import { Title } from './Board';
+
+const TitleInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid#dadde6;
+`;
+const ButtonWrap = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const ContentLabel = styled.label`
+  margin-bottom: 15px;
+  display: block;
+`;
+const InputWrap = styled.div`
+  margin-bottom: 20px;
+  width: 100%;
+`;
 
 const PostEdit = () => {
   const navigate = useNavigate();
@@ -54,28 +77,51 @@ const PostEdit = () => {
   return (
     <>
       <div>
-        <input ref={titleRef}></input>
-        {content.id === '' ? null : (
-          <Editor
-            initialValue={content.content}
-            ref={editorRef} // DOM 선택용 useRef
-            placeholder='내용을 입력해주세요.'
-            previewStyle='vertical' // 미리보기 스타일 지정
-            height='350px' // 에디터 창 높이
-            initialEditType='wysiwyg' // 초기 입력모드 설정(디폴트 markdown)
-            hideModeSwitch={true}
-            language='ko-KR'
-            toolbarItems={[
-              // 툴바 옵션 설정
-              ['image', 'link'],
-            ]}
-          ></Editor>
-        )}
+        <Title>게시글 수정</Title>
+        <InputWrap>
+          <ContentLabel htmlFor='title'>제목</ContentLabel>
+          <TitleInput ref={titleRef}></TitleInput>
+        </InputWrap>
+        <InputWrap>
+          <ContentLabel>내용</ContentLabel>
+          {content.id === '' ? null : (
+            <Editor
+              initialValue={content.content}
+              ref={editorRef} // DOM 선택용 useRef
+              placeholder='내용을 입력해주세요.'
+              previewStyle='vertical' // 미리보기 스타일 지정
+              height='350px' // 에디터 창 높이
+              initialEditType='wysiwyg' // 초기 입력모드 설정(디폴트 markdown)
+              hideModeSwitch={true}
+              language='ko-KR'
+              toolbarItems={[
+                // 툴바 옵션 설정
+                ['image', 'link'],
+              ]}
+            ></Editor>
+          )}{' '}
+        </InputWrap>
       </div>
       {content.id === id ? (
-        <>
-          <button onClick={handleRegisterButton}>수정</button>
-        </>
+        <ButtonWrap>
+          <BasicButton
+            OnClick={() => {
+              navigate(-1);
+            }}
+          >
+            취소
+          </BasicButton>
+          <SolidButton
+            ClassName={'ml_10'}
+            OnClick={() => {
+              if (window.confirm('게시글을 수정하시겠습니까?')) {
+                handleRegisterButton();
+              }
+            }}
+          >
+            수정
+          </SolidButton>
+        </ButtonWrap>
       ) : null}
     </>
   );
