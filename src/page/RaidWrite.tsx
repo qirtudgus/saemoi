b7df4cdc50c50a3cde72245f8934f99e38dba077
qirtudgus <qirtudgus@naver.com>
2022-12-05 18:08:05 +0900

    대비가 너무 강했던 구분선 색상 조정

--- src/page/RaidWrite.tsx
+++ src/page/RaidWrite.tsx
@@ -186,7 +186,7 @@ const HeaderWrap = styled.header`
   height: 60px;
   background: #35363a;
   z-index: 100;
-  border-bottom: 1px solid #e5e7eb;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderColor};
   box-sizing: border-box;
   ${({ theme }) => theme.common.flexCenter};
   margin-bottom: 50px;
