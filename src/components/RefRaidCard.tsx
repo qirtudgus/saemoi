8977babd9209a38196a188c13168a0f782526e2f
qirtudgus <qirtudgus@naver.com>
2022-12-06 20:26:26 +0900

    3분이 지난 레이드인 경우 종료된 레이드라는 창을 출력

--- src/components/RefRaidCard.tsx
+++ src/components/RefRaidCard.tsx
@@ -1,11 +1,13 @@
-import { forwardRef } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import theme from '../layout/theme';
-import { returnDiffTime } from '../util/returnTodayString';
-
-const ListCard = styled.div`
+import { forwardRef, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import React from 'react';
+import moment from 'moment';
+import 'moment/locale/ko';
+const ListCard = styled(motion.div)`
   width: 100%;
   height: auto;
+  position: relative;
   background-color: #575757;
   margin-bottom: 15px;
   border-radius: 5px;
@@ -15,6 +17,23 @@ const ListCard = styled.div`
     word-break: keep-all;
     line-height: 1.4em;
   }
+`;
+
+const ExitListCard = styled(ListCard)``;
+
+const ExitDiv = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.3em;
+  font-weight: bold;
+  color: #fff;
 `;
 
 const ListTop = styled.p`
@@ -59,32 +78,70 @@ const RaidText = styled.span`
 
 const RefRaidCard = forwardRef(function (props: any, ref: any) {
   return (
-    <ThemeProvider theme={theme}>
-      <ListCard ref={ref}>
-        <ListTop as='div'>
-          <div>
-            <span>{props.raidDifficulty}</span>
-            <span>{props.monsterName}</span>
-            <VerticalLine />
-            <span className='raidCode'>{props.raidCode}</span>
-          </div>
-          <ListFooterTime> {returnDiffTime(props.date)}</ListFooterTime>
-        </ListTop>
-        {props.raidText !== '' && (
-          <p>
-            <RaidText>- </RaidText>
-            {props.raidText}
-          </p>
-        )}
-        {props.raidOption !== '' && (
-          <p>
-            <RaidText># </RaidText>
-            {props.raidOption}
-          </p>
-        )}
-      </ListCard>
-    </ThemeProvider>
+    <>
+      {moment().diff(props.date, 'seconds') > 10 ? (
+        <ExitListCard
+          initial={{ opacity: 0.3 }}
+          animate={{ opacity: 1, x: [5, -5, 0] }}
+          transition={{ duration: 0.4 }}
+          ref={ref}
+        >
+          <ExitDiv>종료된 레이드</ExitDiv>
+          <ListTop as='div'>
+            <div>
+              <span>{props.raidDifficulty}</span>
+              <span>{props.monsterName}</span>
+              <VerticalLine />
+              <span className='raidCode'>{props.raidCode}</span>
+            </div>
+            <ListFooterTime> {props.date.slice(-9)}</ListFooterTime>
+          </ListTop>
+          {props.raidText !== '' && (
+            <p>
+              <RaidText>- </RaidText>
+              {props.raidText}
+            </p>
+          )}
+          {props.raidOption !== '' && (
+            <p>
+              <RaidText># </RaidText>
+              {props.raidOption}
+            </p>
+          )}
+        </ExitListCard>
+      ) : (
+        <ListCard
+          initial={{ opacity: 0.3 }}
+          animate={{ opacity: 1, y: [0, 20, 0] }}
+          transition={{ duration: 0.3 }}
+          exit={{ opacity: 0 }}
+          ref={ref}
+        >
+          <ListTop as='div'>
+            <div>
+              <span>{props.raidDifficulty}</span>
+              <span>{props.monsterName}</span>
+              <VerticalLine />
+              <span className='raidCode'>{props.raidCode}</span>
+            </div>
+            <ListFooterTime> {props.date.slice(-9)}</ListFooterTime>
+          </ListTop>
+          {props.raidText !== '' && (
+            <p>
+              <RaidText>- </RaidText>
+              {props.raidText}
+            </p>
+          )}
+          {props.raidOption !== '' && (
+            <p>
+              <RaidText># </RaidText>
+              {props.raidOption}
+            </p>
+          )}
+        </ListCard>
+      )}
+    </>
   );
 });
-
+//리덕스에서 배열을 통째로 바꾸기때문에 react memo가 필요없음
 export default RefRaidCard;
