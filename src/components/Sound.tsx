9d198afb78cdef65a48100b1cf755dd7d6474035
qirtudgus <qirtudgus@naver.com>
2022-12-07 00:29:04 +0900

    사파리 소리재생 이슈 #네번째..
    유즈이펙트를 벗기고 그냥 바로 소켓통신을 연결했다.

--- src/components/Sound.tsx
+++ src/components/Sound.tsx
@@ -3,27 +3,41 @@ import React, { useEffect, useState } from 'react';
 import { socket } from '../App';
 const Sound = () => {
   //   const isPlay = useAppSelector((state) => state.sound);
-  const [audio] = useState(new Audio(audios));
-
+  // const [audio] = useState(new Audio(audios));
+  let audio = new Audio(audios);
   audio.volume = 0.4;
   audio.autoplay = false;
   audio.onended = (e) => {
     console.log('피카추!');
   };
 
-  useEffect(() => {
-    socket.on('newPost', function (payload) {
-      console.log('여기는 사운드 컴포넌트');
-      console.log(payload);
-      if (payload === true) {
-        // audio.play();
-        const promise = audio.play();
-        if (promise !== undefined) {
-          promise.then(() => {}).catch((error) => console.error);
-        }
-      }
-    });
-  }, []);
+  socket.on('newPost', function (payload) {
+    console.log('여기는 사운드 컴포넌트');
+    console.log(payload);
+    if (payload === true) {
+      // audio.play();
+      audio.play();
+      // const promise = audio.play();
+      // if (promise !== undefined) {
+      //   promise.then(() => {}).catch((error) => console.error);
+      // }
+    }
+  });
+
+  // useEffect(() => {
+  //   socket.on('newPost', function (payload) {
+  //     console.log('여기는 사운드 컴포넌트');
+  //     console.log(payload);
+  //     if (payload === true) {
+  //       // audio.play();
+  //       audio.play();
+  //       // const promise = audio.play();
+  //       // if (promise !== undefined) {
+  //       //   promise.then(() => {}).catch((error) => console.error);
+  //       // }
+  //     }
+  //   });
+  // }, []);
 
   return <></>;
 };
