557303a2202eda50eebeca86a824be2627723939
qirtudgus <qirtudgus@naver.com>
2022-12-08 18:57:36 +0900

    레이드카운트 따로 증가

--- src/store/soundSlice.ts
+++ src/store/soundSlice.ts
@@ -3,20 +3,16 @@ import { createSlice } from '@reduxjs/toolkit';
 
 let initialState: boolean = false;
 
-const Sound = createSlice({
-  name: 'Sound',
+const RaidCount = createSlice({
+  name: 'RaidCount',
   initialState,
   reducers: {
-    SoundtDispatch(state: boolean, action) {
+    RaidCountDispatch(state: boolean, action) {
       //[{id:'zxc',socketId:'1234'},{id:'qwe',socketId:'4555'},{id:'asd',socketId:'1222'}];
-      return true;
-    },
-    SoundtFalseDispatch(state: boolean, action) {
-      //[{id:'zxc',socketId:'1234'},{id:'qwe',socketId:'4555'},{id:'asd',socketId:'1222'}];
-      return false;
+      return action.payload;
     },
   },
 });
-export let { SoundtDispatch, SoundtFalseDispatch } = Sound.actions;
+export let { RaidCountDispatch } = RaidCount.actions;
 
-export default Sound.reducer;
+export default RaidCount.reducer;
