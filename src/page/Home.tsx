7c21dcd39aec6fb5857a7b65e0f038c3085de086
qirtudgus <qirtudgus@naver.com>
2022-12-08 09:21:15 +0900

    상호작용 유도를 위한 Home 컴포넌트 수정

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,32 +1,75 @@
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import RaidBoard from './RaidBoard';
-import RealTimeRaidBoard from './RealTimeRaidBoard';
-
+import styled, { keyframes } from 'styled-components';
+import { useAppSelector } from '../store/store';
+import { motion } from 'framer-motion';
+import 로고 from '../img/logo_s.svg';
 const Bg = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
   z-index: 100;
-  background: #575757;
+  background: ##35363a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 `;
-const Button = styled.button`
-  width: 200px;
-  height: 100px;
+const Button = styled(motion.button)`
+  cursor: pointer;
+  width: 130px;
+  height: 55px;
+  background: #ef5a34;
+  font-size: 1.2em;
+  color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 9px 5px rgba(0, 0, 0, 0.2);
 `;
+
+const usersCountAni = keyframes`
+ 0%{transform:translateY(3px)}
+ 50%{transform:translateY(-3px)}
+ 100%{transform:translateY(3px)}
+`;
+const UsersCount = styled.span`
+  display: inline-block;
+  animation: ${usersCountAni} 1s ease infinite;
+  color: #ef5a34;
+  font-weight: bold;
+`;
+
+const UsersCountText = styled.p`
+  font-size: 2.4em;
+  margin: 20px;
+`;
+
+const SelfText = styled.p``;
+
+const Img = styled.img`
+  width: 150px;
+`;
+
 const Home = () => {
   const navigate = useNavigate();
+  const usersCount = useAppSelector((state) => state.userCount);
 
   return (
-    <Bg>
-      <Button
-        onClick={() => {
-          navigate('/realtimeraidboard');
-        }}
-      >
-        입장하기
-      </Button>
-    </Bg>
+    <>
+      <Bg>
+        <Img src={로고}></Img>
+        <SelfText>실시간 테라레이드 파티원 모집!</SelfText>
+        <UsersCountText>
+          현재 접속자 <UsersCount>{usersCount}</UsersCount> 명
+        </UsersCountText>
+        <Button
+          whileHover={{ scale: 1.05 }}
+          onClick={() => {
+            navigate('/realtimeraidboard');
+          }}
+        >
+          입장하기
+        </Button>
+      </Bg>
+    </>
   );
 };
 
