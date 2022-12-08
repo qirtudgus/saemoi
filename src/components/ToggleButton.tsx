4c6324d5ebda4d1322b8f6b872e97c3f46c59f35
qirtudgus <qirtudgus@naver.com>
2022-12-08 09:19:27 +0900

    설정 on/off용 토글버튼 생성

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+const spring = {
+  type: 'spring',
+  stiffness: 300,
+  damping: 20,
+};
+
+const Switch = styled(motion.div)`
+  width: 65px;
+  height: 30px;
+  background-color: rgba(255, 255, 255, 0.4);
+  display: flex;
+  justify-content: flex-start;
+  border-radius: 50px;
+  padding: 7px;
+  cursor: pointer;
+  align-items: center;
+  &[data-ison='true'] {
+    justify-content: flex-end;
+    background-color: #ef5a34;
+  }
+
+  & .handle {
+    width: 22px;
+    height: 22px;
+    background-color: white;
+    border-radius: 40px;
+  }
+`;
+
+export default function ToggleButton(props: any) {
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleSwitchOn = () => {
+    props.OnFunc();
+    setIsOn(!isOn);
+  };
+  const toggleSwitchOff = () => {
+    props.OffFunc();
+    setIsOn(!isOn);
+  };
+  useEffect(() => {
+    console.log(props.isOn);
+    if (props.isOn === 'true') {
+      setIsOn(true);
+    }
+  }, []);
+
+  return (
+    <>
+      <Switch
+        className='switch'
+        data-ison={isOn}
+        onClick={isOn ? toggleSwitchOff : toggleSwitchOn}
+      >
+        <motion.div className='handle' />
+      </Switch>
+    </>
+  );
+}
