5d21dec82a99f4b7c07fcaec1964b0e4ea03370f
qirtudgus <qirtudgus@naver.com>
2022-12-11 01:40:50 +0900

    환경변수를 참조하지못해 로그인유지에 오류가 나던것 해결

--- src/util/createAccessToken.ts
+++ src/util/createAccessToken.ts
@@ -1,11 +1,10 @@
 import jwt from 'jsonwebtoken';
-const SECRET_TOKEN: string = process.env.SECRET_TOKEN!;
 type TokenType = string | unknown;
 
 export const createAccessToken = (id: string | any): TokenType => {
   try {
-    console.log(`${id}님의 토큰 생성`);
-    return jwt.sign({ id }, SECRET_TOKEN, { expiresIn: '10h' });
+    console.log(`${id}님의 액세스 토큰 생성`);
+    return jwt.sign({ id }, process.env.SECRET_TOKEN!, { expiresIn: '10h' });
   } catch (err) {
     return err;
   }
@@ -13,8 +12,8 @@ export const createAccessToken = (id: string | any): TokenType => {
 
 export const createRefreshToken = (id: string | any): TokenType => {
   try {
-    console.log(`${id}님의 토큰 생성`);
-    return jwt.sign({}, SECRET_TOKEN, { expiresIn: '360h' });
+    console.log(`${id}님의 리프레쉬토큰 생성`);
+    return jwt.sign({}, process.env.SECRET_TOKEN!, { expiresIn: '360h' });
   } catch (err) {
     return err;
   }
--- src/util/verifyToken.ts
+++ src/util/verifyToken.ts
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken';
-const SECRET_TOKEN: string = process.env.SECRET_TOKEN!;
+// const SECRET_TOKEN: string = process.env.SECRET_TOKEN!;
 type TokenType = string | unknown;
 
 //유효할 시 토큰을 분해한값을, 만료 시 null 리턴
 const verifyToken = (token: string): TokenType => {
   try {
-    return jwt.verify(token, SECRET_TOKEN);
+    return jwt.verify(token, process.env.SECRET_TOKEN!);
   } catch (err) {
     return null;
   }
