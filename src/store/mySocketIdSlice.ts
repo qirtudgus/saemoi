4f69e8b632455a45005cf9069b8d1c30d3f10b18
qirtudgus <qirtudgus@naver.com>
2022-12-14 23:42:11 +0900

    소켓id를 담고있는 스토어 추가

--- src/store/mySocketIdSlice.ts
+++ src/store/mySocketIdSlice.ts
@@ -1,18 +1,18 @@
 //View컴포넌트에서 useEffect를 사용하는것으로 변경하여 현재 사용하지않는 파일임
 import { createSlice } from '@reduxjs/toolkit';
 
-let initialState: number = 0;
+let initialState: string = '';
 
-const SoundNumber = createSlice({
-  name: 'SoundNumber',
+const MySocketId = createSlice({
+  name: 'MySocketId',
   initialState,
   reducers: {
-    SoundNumberDispatch(state: number, action) {
+    MySocKetIdDispatch(state: string, action) {
       //[{id:'zxc',socketId:'1234'},{id:'qwe',socketId:'4555'},{id:'asd',socketId:'1222'}];
       return action.payload;
     },
   },
 });
-export let { SoundNumberDispatch } = SoundNumber.actions;
+export let { MySocKetIdDispatch } = MySocketId.actions;
 
-export default SoundNumber.reducer;
+export default MySocketId.reducer;
