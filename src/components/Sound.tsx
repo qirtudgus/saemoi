6a55f7df3d8734b5316c891ddf881af663854cc3
qirtudgus <qirtudgus@naver.com>
2022-12-20 18:22:12 +0900

    pc알림 클릭 시 해당 브라우저가 포커싱 되도록 구현

--- src/components/Sound.tsx
+++ src/components/Sound.tsx
@@ -18,13 +18,20 @@ const Sound = () => {
       //진동은 지원되지 않는 기기가 많아서 꼭 소리재생 후에 실행
       navigator.vibrate([150, 80, 150]);
     }
-    // if (Notification.permission === 'granted') {
-    //   const notification = new Notification('새로운 레이드가 등록되었습니다!', { tag: '새로움', renotify: true });
-    //   notification.onclick = (event) => {
-    //     event.preventDefault(); // prevent the browser from focusing the Notification's tab
-    //     window.open('http://www.mozilla.org', '_blank');
-    //   };
-    // }
+    if (Notification.permission === 'granted') {
+      const notification = new Notification('새로운 레이드가 등록되었습니다!', {
+        tag: '새로움',
+        renotify: true,
+        silent: true,
+      });
+      //알람을 클릭 시 사이트를 포커싱해준다.
+      notification.onclick = (event) => {
+        event.preventDefault(); // prevent the browser from focusing the Notification's tab
+        // window.open('http://www.mozilla.org', '_blank');
+
+        window.focus();
+      };
+    }
   };
 
   useEffect(() => {
